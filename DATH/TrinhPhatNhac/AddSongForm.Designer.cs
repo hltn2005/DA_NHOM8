7f0da2205707368df5aea00ad7c7897eae30ce4a
letrieuthanhphuong <letrieuthanhphuong933@gmail.com>
2025-12-01 23:17:08 +0700

    Update & fix

--- DATH/TrinhPhatNhac/AddSongForm.Designer.cs
+++ DATH/TrinhPhatNhac/AddSongForm.Designer.cs
@@ -28,6 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
+			this.dgvAddSong = new System.Windows.Forms.DataGridView();
+			this.ColSongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ColSingerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.btnThem = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvAddSong)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvAddSong
+			// 
+			this.dgvAddSong.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvAddSong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvAddSong.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColSongName,
+            this.ColSingerName});
+			this.dgvAddSong.Location = new System.Drawing.Point(12, 12);
+			this.dgvAddSong.Name = "dgvAddSong";
+			this.dgvAddSong.RowHeadersWidth = 51;
+			this.dgvAddSong.RowTemplate.Height = 24;
+			this.dgvAddSong.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dgvAddSong.Size = new System.Drawing.Size(688, 431);
+			this.dgvAddSong.TabIndex = 18;
+			// 
+			// ColSongName
+			// 
+			this.ColSongName.DataPropertyName = "SongName";
+			this.ColSongName.HeaderText = "Tên Bài Hát";
+			this.ColSongName.MinimumWidth = 6;
+			this.ColSongName.Name = "ColSongName";
+			// 
+			// ColSingerName
+			// 
+			this.ColSingerName.DataPropertyName = "SingerName";
+			this.ColSingerName.HeaderText = "Ca Sĩ";
+			this.ColSingerName.MinimumWidth = 6;
+			this.ColSingerName.Name = "ColSingerName";
+			// 
+			// btnThem
+			// 
+			this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnThem.Location = new System.Drawing.Point(580, 449);
+			this.btnThem.Name = "btnThem";
+			this.btnThem.Size = new System.Drawing.Size(85, 51);
+			this.btnThem.TabIndex = 19;
+			this.btnThem.Text = "Thêm";
+			this.btnThem.UseVisualStyleBackColor = false;
+			this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+			// 
+			// AddSongForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(712, 512);
+			this.Controls.Add(this.btnThem);
+			this.Controls.Add(this.dgvAddSong);
+			this.Name = "AddSongForm";
+			this.Text = "AddSongForm";
+			this.Load += new System.EventHandler(this.AddSongForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvAddSong)).EndInit();
+			this.ResumeLayout(false);
+=======
             this.dgvAddSong = new System.Windows.Forms.DataGridView();
             this.ColSongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColSingerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -88,6 +150,7 @@
             this.Load += new System.EventHandler(this.AddSongForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAddSong)).EndInit();
             this.ResumeLayout(false);
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
 
         }
 
--- DATH/TrinhPhatNhac/PlaylistForm.cs
+++ DATH/TrinhPhatNhac/PlaylistForm.cs
@@ -10,6 +10,19 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+<<<<<<< HEAD
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace TrinhPhatNhac
+{
+    public partial class btnFindSongs : Form
+    {
+
+        List<Song> songList = new List<Song>();
+        List<Song> songListRoot = new List<Song>();
+        private PlayListSong PlayListSong = new PlayListSong();
+        public btnFindSongs(List<Song> songlistroot, List<Song> songlist)
+=======
 
 namespace TrinhPhatNhac
 {
@@ -19,19 +32,28 @@ namespace TrinhPhatNhac
         List<Song> songList = new List<Song>();
         List<Song> songListRoot=new List<Song>();
         public PlaylistForm(List<Song> songlistroot,List<Song> songlist)
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
         {
             InitializeComponent();
             songList = songlist;
             songListRoot = songlistroot;
             dgvPlayList.AutoGenerateColumns = false;
+<<<<<<< HEAD
+            dgvPlayList.DataSource = songlist;
+=======
             dgvPlayList.DataSource=songlist;
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
             dgvPlayList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
 
         }
         #region Button
         private void btnPlaylistManager2_Click(object sender, EventArgs e)
         {
+<<<<<<< HEAD
+            PlaylistManagerForm qlPlaylist = new PlaylistManagerForm(songListRoot, songList);
+=======
             PlaylistManagerForm qlPlaylist = new PlaylistManagerForm( songListRoot,songList);
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
             qlPlaylist.ShowDialog();
             // điều kiện kiểm tra nếu playlist đã chọn rỗng hoặc null thì dgv sẽ không có dữ liệu
             if (songList == null || songList.Count == 0)
@@ -39,7 +61,11 @@ namespace TrinhPhatNhac
                 dgvPlayList.DataSource = null;
                 return;
             }
+<<<<<<< HEAD
+            if (qlPlaylist.SelectedPlayList != null && qlPlaylist.SelectedPlayList.PlayList.Count > 0)
+=======
             if (qlPlaylist.SelectedPlayList!=null&&qlPlaylist.SelectedPlayList.PlayList.Count>0)
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
             {
                 songList.Clear();
                 songList.AddRange(qlPlaylist.SelectedPlayList.PlayList);// addrange thêm nhiều bài hát 1 lần 
@@ -50,9 +76,15 @@ namespace TrinhPhatNhac
 
         private void btnAddSong_Click(object sender, EventArgs e)
         {
+<<<<<<< HEAD
+            AddSongForm newForm = new AddSongForm(songListRoot, songList);
+            newForm.ShowDialog();
+            if (newForm.CheckSelect())// nếu danh sách có thêm bài hát mới thì cập nhật lại data cho dgv
+=======
             AddSongForm newForm = new AddSongForm(songListRoot,songList);
             newForm.ShowDialog();
             if(newForm.CheckSelect())// nếu danh sách có thêm bài hát mới thì cập nhật lại data cho dgv
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
             {
                 dgvPlayList.DataSource = null;
                 dgvPlayList.DataSource = songList;
@@ -64,8 +96,13 @@ namespace TrinhPhatNhac
             if (dgvPlayList.SelectedRows.Count > 0)
             {
                 DataGridViewRow selectedRow = dgvPlayList.SelectedRows[0];
+<<<<<<< HEAD
+                string songRemove = selectedRow.Cells[0].Value.ToString();
+                DialogResult result = MessageBox.Show($"Bạn có chắc chăn muốn xóa bài {songRemove} không ?", "Thông báo",
+=======
                 string songRemove= selectedRow.Cells[0].Value.ToString();
                 DialogResult result= MessageBox.Show($"Bạn có chắc chăn muốn xóa bài {songRemove} không ?", "Thông báo",
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
                     MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (result == DialogResult.Yes)
                 {
@@ -81,5 +118,81 @@ namespace TrinhPhatNhac
         {
             //dgvPlayList.DataSource=currentList.ConvertToList();
         }
+<<<<<<< HEAD
+
+        private List<Song> FindSongs(string searchKeyword)
+        {
+            List<Song> foundSongs = new List<Song>();
+            if (string.IsNullOrEmpty(searchKeyword))
+                return foundSongs;
+            foreach (Song song in songList)
+            {
+                if ((song.SongName != null && song.SongName.ToLower().Contains(searchKeyword))
+                    || (song.SingerName != null && song.SingerName.ToLower().Contains(searchKeyword)))
+                {
+                    foundSongs.Add(song);
+                }
+            }
+            return foundSongs;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string keyword = txtFindSongs.Text.Trim().ToLower();
+            if (string.IsNullOrEmpty(keyword))
+                dgvPlayList.DataSource = songList;
+            else
+            {
+                List<Song> foundSongs = FindSongs(keyword);
+                dgvPlayList.DataSource = foundSongs;
+            }
+        }
+
+        private int GetSelectedRowIndex()
+        {
+            if (dgvPlayList.SelectedRows.Count == 1)
+                return dgvPlayList.SelectedRows[0].Index;
+            return -1;
+        }
+        private void SwapSong(List<Song> list, int indexA, int indexB)
+        {
+            Song song = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = song;
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = GetSelectedRowIndex();
+            if (selectedIndex > 0)
+            {
+                SwapSong(songList, selectedIndex, selectedIndex - 1);
+
+                dgvPlayList.DataSource = null;
+                dgvPlayList.DataSource = songList;
+
+                //Trỏ lại chỗ được chọn
+                dgvPlayList.Rows[selectedIndex].Selected = false;
+                dgvPlayList.Rows[selectedIndex - 1].Selected = true;
+            }
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = GetSelectedRowIndex();
+            if (selectedIndex != -1 && selectedIndex < songList.Count - 1)
+            {
+                SwapSong(songList, selectedIndex, selectedIndex + 1);
+
+                dgvPlayList.DataSource = null;
+                dgvPlayList.DataSource = songList;
+
+                //Trỏ lại chỗ được chọn
+                dgvPlayList.Rows[selectedIndex].Selected = false;
+                dgvPlayList.Rows[selectedIndex + 1].Selected = true;
+            }
+        }
+=======
+>>>>>>> bb97091cb84098b8a9cde50f6ce5deb0ecf38d7c
     }
 }
